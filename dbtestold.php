3591f6c3f391f4fa8fc321fce8b7c8ac4c4f8aa6
Georg <chillageorg@hispeed.ch>
2014-11-03 17:35:13 +0100

    db-change

---
+++ dbtestold.php
@@ -0,0 +1,33 @@
+<?php
+
+define("DBServer", "localhost");
+define("DBServerPW", "test");
+define("DBServerUser", "root");
+define("DBInternal", "playground");
+// Verbindung aufbauen, auswählen einer Datenbank
+$link = mysql_connect(DBServer, DBServerUser, DBServerPW)
+    or die("Keine Verbindung moeglich: " . mysql_error());
+echo "Verbindung zum Datenbankserver erfolgreich";
+mysql_select_db(DBInternal) or die("Auswahl der Datenbank fehlgeschlagen");
+
+// Ausführen einer SQL-Anfrage
+$query = "SELECT * FROM test";
+$result = mysql_query($query) or die("Anfrage fehlgeschlagen: " . mysql_error());
+
+// Ausgabe der Ergebnisse in HTML
+echo "<table>\n";
+while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
+    echo "\t<tr>\n";
+    foreach ($line as $col_value) {
+        echo "\t\t<td>$col_value</td>\n";
+    }
+    echo "\t</tr>\n";
+}
+echo "</table>\n";
+
+// Freigeben des Resultsets
+mysql_free_result($result);
+
+// Schließen der Verbinung
+mysql_close($link);
+?>
